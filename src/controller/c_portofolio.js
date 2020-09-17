b08816d5a9e1ac8ed12006576b3aee53a266f965
Muhammad Nur Hidayat <dayaters22@gmail.com>
2020-09-17 17:30:10 +0800

    Worker - add coloumn link & repo in table

--- src/controller/c_portofolio.js
+++ src/controller/c_portofolio.js
@@ -2,13 +2,15 @@ const helper = require("../helper/helper");
 const { postPortofolio } = require('../model/m_portofolio')
 module.exports = {
 	postPortofolio: async (request, response) => {
-    const { user_id, portofolio_name } = request.body
+    const { user_id, portofolio_name, portofolio_type, portofolio_repository } = request.body
     const portofolio_image = request.file
 
     if (
       user_id == '' || user_id == undefined ||
       portofolio_name == '' || portofolio_name == undefined ||
-      portofolio_image == '' || portofolio_image == undefined
+      portofolio_image == '' || portofolio_image == undefined ||
+      portofolio_type == '' || portofolio_type == undefined ||
+      portofolio_repository == '' || portofolio_repository == undefined
       ) {
       return helper.response(response, 400, "Your da is not complete")
     }
@@ -17,7 +19,9 @@ module.exports = {
       const setData = {
         user_id,
         portofolio_name,
-        portofolio_image: portofolio_image === undefined ? '' : portofolio_image.filename
+        portofolio_image: portofolio_image === undefined ? '' : portofolio_image.filename,
+        portofolio_type,
+        portofolio_repository
       }
       const result = await postPortofolio(setData)
       return helper.response(response, 201, "Seuccess post portofolio", result)
@@ -25,4 +29,9 @@ module.exports = {
     	return helper.response(response, 400, "Bad request", e)
     }
   },
+  patchPortofolio: async (request, response) => {
+    console.log(request.body)
+    console.log(request.params)
+    console.log(request.file)
+  }
 }
--- src/routes/r_portofolio.js
+++ src/routes/r_portofolio.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
-const { postPortofolio } = require('../controller/c_portofolio')
+const { postPortofolio, patchPortofolio } = require('../controller/c_portofolio')
 const { authorization } = require('../middleware/auth')
 const uploadImage = require('../middleware/multer_portofolio')
 
 router.post("/", authorization, uploadImage, postPortofolio);
+router.patch("/:id", authorization, uploadImage, patchPortofolio);
 
 module.exports = router
