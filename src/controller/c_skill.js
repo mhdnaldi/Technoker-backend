d0234fc1cc1e870a2bfeac4758af99cfdb34cde0
Arizal Sykes <arizalinc@gmail.com>
2020-09-20 22:24:05 +0700

    feat: add count skill to user_skill

--- src/controller/c_skill.js
+++ src/controller/c_skill.js
@@ -1,48 +1,64 @@
 const helper = require("../helper/helper");
-const { postSkill, deleteSkill, getWorkerSkills, checkWorkerSkill } = require("../model/m_skill");
-const { getWorkerById } = require("../model/m_worker");
+const {
+  postSkill,
+  deleteSkill,
+  getWorkerSkills,
+  checkWorkerSkill,
+} = require("../model/m_skill");
+const { getWorkerById, patchDataWorker } = require("../model/m_worker");
 
 module.exports = {
-    postSkill: async (request, response) => {
-        const { skill_name, user_id } = request.body;
-        if (
-            user_id === "" || user_id === undefined ||
-            skill_name === "" || skill_name === null
-        ) {
-            return helper.response(
-                response,
-                400,
-                "Data must be complete"
-            );
+  postSkill: async (request, response) => {
+    const { skill_name, user_id } = request.body;
+    if (
+      user_id === "" ||
+      user_id === undefined ||
+      skill_name === "" ||
+      skill_name === null
+    ) {
+      return helper.response(response, 400, "Data must be complete");
+    }
+    try {
+      checkId = await getWorkerById(user_id);
+      if (checkId.length > 0) {
+        checkSkill = await checkWorkerSkill(skill_name, user_id);
+        if (checkSkill.length > 0) {
+          return helper.response(
+            response,
+            200,
+            `The ${skill_name} skill for this user is already set`
+          );
+        } else {
+          const setData = {
+            skill_name,
+            user_id,
+          };
+          const setData2 = {
+            user_skill: +1,
+          };
+          const result = await postSkill(setData);
+          const updateSkill = await patchDataWorker(setData2, user_id);
+          return helper.response(response, 200, "Success post skill", result);
         }
-        try {
-            checkId = await getWorkerById(user_id)
-            if (checkId.length > 0) {
-                checkSkill = await checkWorkerSkill(skill_name, user_id)
-                if (checkSkill.length > 0) {
-                    return helper.response(response, 200, `The ${skill_name} skill for this user is already set`);
-                } else {
-                    const setData = {
-                        skill_name,
-                        user_id,
-                    };
-                    const result = await postSkill(setData);
-                    return helper.response(response, 200, "Success post skill", result);
-                }
-            } else {
-                return helper.response(response, 400, `Worker with ID ${user_id} is not Found!`);
-            }
-        } catch (error) {
-            return helper.response(response, 400, "Bad request");
-        }
-    },
-    deleteSkill: async (request, response) => {
-        try {
-            const { id } = request.params;
-            const result = await deleteSkill(id);
-            return helper.response(response, 200, "Skill deleted", result);
-        } catch (error) {
-            return helper.response(response, 400, "Bad Request", error);
-        }
-    },
+      } else {
+        return helper.response(
+          response,
+          400,
+          `Worker with ID ${user_id} is not Found!`
+        );
+      }
+    } catch (error) {
+      console.log(error);
+      //   return helper.response(response, 400, "Bad request");
+    }
+  },
+  deleteSkill: async (request, response) => {
+    try {
+      const { id } = request.params;
+      const result = await deleteSkill(id);
+      return helper.response(response, 200, "Skill deleted", result);
+    } catch (error) {
+      return helper.response(response, 400, "Bad Request", error);
+    }
+  },
 };
